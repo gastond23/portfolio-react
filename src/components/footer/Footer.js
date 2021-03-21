39ecfd505d61306976b6d9dfa96d5d83538fd0e7
gastond23 <gaston.dandre@gmail.com>
2021-03-21 14:41:56 -0300

    agregado footer & mobile

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import "./Footer.css"
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <div className="footer-info">
+                <h1>Gaston Dandre</h1>
+                <p>Neuquén Argentina</p>
+            </div>
+            <div className="footer-contact">
+                <h3>Contact me</h3>
+                <p>And let's get down to work</p>
+            </div>
+            <div className="footer-sns">
+                <div className="desing-by">
+                    Design by Gaston Dandre
+                </div>
+                <div className="sns-links">
+                    <a href="https://github.com/gastond23" target="_blank" rel="noopener noreferrer">
+                        <i class="fab fa-github"></i>
+                    </a>
+                    <a href="https://www.linkedin.com/in/gaston-dandre-6971801ab" target="_blank" rel="noopener noreferrer">
+                        <i class="fab fa-linkedin"></i>
+                    </a>
+                    <a href="https://www.instagram.com/gastond23/" target="_blank" rel="noopener noreferrer">
+                        <i class="fab fa-instagram"></i>
+                    </a>
+                    <a href="https://twitter.com/DandreGaston" target="_blank" rel="noopener noreferrer">
+                        <i class="fab fa-twitter"></i>
+                    </a>
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
